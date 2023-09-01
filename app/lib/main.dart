10c5d36f4ebc97c79cfa8000b47f813fb9ada1fe
YeJun, Jung <yejun614@naver.com>
2023-09-01 10:47:42 +0900

    Chore: setup flutter app

---
+++ app/lib/main.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(CoffeeMoneyApp());
+}
+
+class CoffeeMoneyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Hello"),
+      ),
+    );
+  }
+}
